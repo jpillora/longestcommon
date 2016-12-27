34c08c48a875dc2599603638ee89ff3fb30d13a3
Jaime Pillora <dev@jpillora.com>
2016-12-28 10:31:41 +1100

    init

---
+++ lcfix.go
@@ -0,0 +1,58 @@
+package lcfix
+
+//LongestCommonPrefix of the provided strings
+func LongestCommonPrefix(strs []string) string {
+	return longestCommonXfix(strs, true)
+}
+
+//LongestCommonSuffix of the provided strings
+func LongestCommonSuffix(strs []string) string {
+	return longestCommonXfix(strs, false)
+}
+
+func longestCommonXfix(strs []string, pre bool) string {
+	//short-circuit empty list
+	if len(strs) == 0 {
+		return ""
+	}
+	xfix := strs[0]
+	//short-circuit single-element list
+	if len(strs) == 1 {
+		return xfix
+	}
+	//compare first to rest
+	for _, str := range strs[1:] {
+		//maximum possible length
+		xfixl := len(xfix)
+		strl := len(str)
+		//short-circuit empty strings
+		if xfixl == 0 || strl == 0 {
+			return ""
+		}
+		maxl := xfixl
+		if strl < maxl {
+			maxl = strl
+		}
+		//compare letters
+		if pre {
+			//prefix, iterate left to right
+			for i := 0; i < maxl; i++ {
+				if xfix[i] != str[i] {
+					xfix = xfix[:i]
+					break
+				}
+			}
+		} else {
+			//suffix, iternate right to left
+			for i := 0; i < maxl; i++ {
+				xi := xfixl - i - 1
+				si := strl - i - 1
+				if xfix[xi] != str[si] {
+					xfix = xfix[xi+1:]
+					break
+				}
+			}
+		}
+	}
+	return xfix
+}
